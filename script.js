8e333377cf0aa3741052d7444e934dd03395e6cd
vicdd <victorrobcam@gmail.com>
2023-07-04 09:34:04 -0300

    added hover effect

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ function createGrid() {
     let grid = document.querySelector(".grid");
     for (let i = 0; i < gridSize * gridSize; i++) {
         let cell = document.createElement('div');
-        cell.classList.add('cell')
+        cell.classList.add('cell');
+        cell.addEventListener('mouseout',
+            () => cell.style.backgroundColor = "black"
+        );
         grid.appendChild(cell);
     }
 }
