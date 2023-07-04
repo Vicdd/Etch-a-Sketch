2785dc461e0592ab32ef34e699ab19568b9904db
vicdd <victorrobcam@gmail.com>
2023-07-04 09:21:58 -0300

    added basic grid layout

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
+const gridSize = 16;
+
 function createGrid() {
-    let row = [];
+    let grid = document.querySelector(".grid");
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        let cell = document.createElement('div');
+        cell.classList.add('cell')
+        grid.appendChild(cell);
+    }
 }
 
 createGrid();
